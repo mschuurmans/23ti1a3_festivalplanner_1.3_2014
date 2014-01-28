82ce9506677715af7faf5a18ff97fd6835680099
rudyjason <rudyjason.ep@hotmail.com>
2014-01-28 11:30:50 +0100

    Test
    klein test bestandje, om GitHub te testen

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.doeIets();
+	}
+
+	public void doeIets() {
+		System.out.println("Hello world");
+	}
+	
+}
