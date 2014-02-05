2ea6e2d096f67facc653491a870454325ec48af3
Jordy Sipkema <jordysipkema@gmail.com>
2014-02-05 10:06:19 +0100

    Panel now supports custom layoutmanagers

--- src/src/nl/avans/festivalplanner/view/Panel.java
+++ src/src/nl/avans/festivalplanner/view/Panel.java
@@ -1,5 +1,6 @@
 package nl.avans.festivalplanner.view;
 
+import java.awt.LayoutManager;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
@@ -9,6 +10,10 @@ public abstract class Panel extends JPanel implements ActionListener
 	public Panel()
 	{
 		super(null);
+	}
+	
+	public Panel(LayoutManager manager){
+		super(manager);
 	}
 	
 	private static final long serialVersionUID = 3132394452901507984L;
