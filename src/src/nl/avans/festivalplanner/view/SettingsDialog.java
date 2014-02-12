ed3e7690a4f132fd3d4396d84e216ef234234bec
Roald Nefs <roald.nefs@live.nl>
2014-02-12 10:41:41 +0100

    SettingsDialog Layout
    SettingsDialog Layout, not finished!

--- src/src/nl/avans/festivalplanner/view/SettingsDialog.java
+++ src/src/nl/avans/festivalplanner/view/SettingsDialog.java
@@ -1,14 +1,14 @@
 package nl.avans.festivalplanner.view;
 
-import java.awt.Frame;
-import java.awt.GridLayout;
-
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import nl.avans.festivalplanner.utils.Enums.Text;
 
 public class SettingsDialog extends JDialog
@@ -34,14 +34,21 @@ public class SettingsDialog extends JDialog
 	{
 		this.setTitle(Text.Settings.toString());
 		this.setSize(300, 400);
-		this.setLayout(new GridLayout(4, 2));
-		this.add(lblLanguage);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		lblLanguage.setBounds(10,20,50,20);
+		panel.add(lblLanguage);
 		comLanguages = new JComboBox(languages);
 		comLanguages.setSelectedIndex(0);
-		this.add(comLanguages);
-		this.add(lblMap);
-		this.add(imgMap);
-		this.add(btnChangeMap);
-		this.add(btnRemoveMap);
+		comLanguages.setBounds(150,20,100,20);
+		panel.add(comLanguages);
+		lblMap.setBounds(10,50,50,20);
+		panel.add(lblMap);
+		//this.add(imgMap); //TODO: afbeelding toevoegen
+		btnChangeMap.setBounds(150,180,120,25);
+		btnRemoveMap.setBounds(150,210,120,25);
+		panel.add(btnChangeMap);
+		panel.add(btnRemoveMap);
+		this.add(panel);
 	}
 }
