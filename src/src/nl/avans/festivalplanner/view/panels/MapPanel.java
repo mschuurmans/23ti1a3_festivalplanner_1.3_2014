c1e5c3f5bd17049c1fef63b56e673c3ded4ba530
Michiel Schuurmans <michiel@codox.org>
2014-02-24 15:21:27 +0100

    Changed the syntaxes for mappanel so they conform code convention

--- src/src/nl/avans/festivalplanner/view/panels/MapPanel.java
+++ src/src/nl/avans/festivalplanner/view/panels/MapPanel.java
@@ -16,52 +16,66 @@ import nl.avans.festivalplanner.model.FestivalHandler;
 import nl.avans.festivalplanner.utils.Enums.Text;
 import nl.avans.festivalplanner.view.Panel;
 
-public class MapPanel extends Panel implements ActionListener, Serializable {
+public class MapPanel extends Panel implements ActionListener, Serializable 
+{
+	private static final long serialVersionUID = 5360144595476066228L;
+	
 	private Image _image;
 	private String _imageSource;
 	private File _imageFile;
 	private final JFileChooser _fc = new JFileChooser(new File("resources"));
-	
-	public MapPanel() {
+
+	public MapPanel() 
+	{
 		_imageSource = FestivalHandler.Instance().getMap();
 		_imageFile = new File(_imageSource);
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		try {
+		try
+		{
 			_image = ImageIO.read(_imageFile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
 		}
+		
 		JButton _mapButton = new JButton(Text.LoadMap.toString());
 		_mapButton.addActionListener(this);
 		add(_mapButton);
 	}
-	
-	protected void paintComponent(Graphics g) {
+
+	protected void paintComponent(Graphics g) 
+	{
 		super.paintComponent(g);
 		repaint();
 		g.drawImage(_image, 0, 0, getWidth(), getHeight(), null);
-		
 	}
 
 	@Override
-	public Panel getPanel() {
+	public Panel getPanel() 
+	{
 		return this;
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) 
+	{
 		int returnVal = _fc.showOpenDialog(this);
 
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
+		if (returnVal == JFileChooser.APPROVE_OPTION) 
+		{
 			_imageFile = _fc.getSelectedFile();
 			_imageSource = _imageFile.toString();
-			try {
+			try
+			{
 				_image = ImageIO.read(_imageFile);
-			} catch (IOException e1) {
+			}
+			catch (IOException e1) 
+			{
 				System.out.println("ERROR WITH IMAGE FILE");
 			}
 		}
+		
 		FestivalHandler.Instance().setMap(_imageSource);
 	}
 }
